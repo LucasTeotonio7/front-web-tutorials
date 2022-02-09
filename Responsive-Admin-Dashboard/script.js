dc778a879e86e068757e633909c9b0ea344aaca8
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-02-08 22:22:57 -0300

    created topbar

--- Responsive-Admin-Dashboard/script.js
+++ Responsive-Admin-Dashboard/script.js
@@ -1,3 +1,14 @@
+// menu toggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
+
+
 // add hovered class in selected list item
 let list = document.querySelectorAll('.navigation li');
 
