f6cdf9bbed0dd85838d86770bbe573b8a8ae3eb4
lucasteotonio <lucas.teotonio@e-comprei.com>
2023-12-26 20:49:11 -0300

    feat: embedded Spotify playlist on the page

--- Countdown/scripts.js
+++ Countdown/scripts.js
@@ -1,13 +1,11 @@
 const nextYear = new Date().getFullYear() + 1
-const countDate = new Date('jul 31, 2023 09:00:00').getTime();
+
+
+const countDate = new Date('jan 1,'+nextYear+' 00:00:00').getTime();
 
 function newYear(){
     const now = new Date().getTime();
     gap = countDate - now;
-
-    if(countDate < now) {
-        return
-    }
 
     const second = 1000;
     const minute = second * 60;
@@ -31,25 +29,9 @@ setInterval(function(){
     newYear();
 },1000)
 
+nextYearStr = nextYear+''
+document.getElementById('title').innerHTML =
+    "Contagem Regressiva para<br><span>20<i>"
+    + nextYearStr.substr(2,3)+"</i></span>";
 
 
-document.getElementById('button-play').addEventListener('click', function() {
-    var audio = document.getElementById('audio');
-    audio.play();
-    this.style.display = 'none';
-  
-    var countdownBx = document.getElementsByClassName('countdownBx')[0];
-    countdownBx.style.display = 'flex';
-    countdownBx.classList.add('show-animation');
-  
-    var imgBx = document.getElementsByClassName('imgBx')[0];
-    imgBx.style.display = 'block';
-    imgBx.classList.add('show-animation');
-  
-    setTimeout(function() {
-      countdownBx.classList.add('show');
-      imgBx.classList.add('show');
-    }, 100); // Aguarde 100 milissegundos antes de adicionar a classe "show"
-  
-    this.parentNode.remove();
-  });
